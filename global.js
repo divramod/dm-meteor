89a5777ced8d5349beaf5e090e03db523239ec1d
divramod <divramod@gmail.de>
2015-09-02 00:41:01 +0200

    adapted to getCommonTasks

--- global.js
+++ global.js
@@ -1,9 +1,6 @@
-var colors = require("colors");
+
 var co = require("co");
-require("shelljs/global");
 var jobs = {};
-var result = {};
-var module_path = __dirname;
 
 // =========== [ job.index() ] ===========
 jobs.index = co.wrap(function*() {
@@ -12,45 +9,24 @@ jobs.index = co.wrap(function*() {
         // =========== [ get params from user input ] ===========
         var argv2 = process.env.dmnJob || process.argv[2] || "help";
 
-        // =========== [ help ] ===========
-        if (["help", "-help", "h", "-h"].indexOf(argv2) > -1) {
-            var task = require("./tasks/help/index.js");
-            yield task.start(module_path);
-        }
         // =========== [ test ] ===========
-        else if (["test", "-test", "t", "-t"].indexOf(argv2) > -1) {
+        if (["test", "-test", "t", "-t"].indexOf(argv2) > -1) {
             var task = require("./tasks/test/index.js");
-            yield task.start();
-        }
-        // =========== [ todo ] ===========
-        else if (["todo"].indexOf(argv2) > -1) {
-            require("dm-npm").todo(__dirname);
-        }
-        // =========== [ idea ] ===========
-        else if (["idea"].indexOf(argv2) > -1) {
-            require("dm-npm").idea(__dirname);
-        }
-        // =========== [ prompt ] ===========
-        else if (["prompt","p"].indexOf(argv2) > -1) {
-            require("dm-npm").prompt(__dirname);
-        }
-        // =========== [ test ] ===========
-        else if (["config", "-config"].indexOf(argv2) > -1) {
-            var task = require("./tasks/config/index.js");
-            yield task.start();
-        }
-        // =========== [ test ] ===========
-        else if (["createKey", "key"].indexOf(argv2) > -1) {
-            var task = require("./tasks/createKey/index.js");
-            yield task.start();
-        }
-        // =========== [ test ] ===========
-        else if (["buildApp", "build"].indexOf(argv2) > -1) {
-            var task = require("./tasks/buildApp/index.js");
             yield task.start();
         }
 
         // automatically add tasks here
+
+        // =========== [ createKey ] ===========
+        else if (["createKey", "key"].indexOf(argv2) > -1) {
+            var task = require("./tasks/createKey/index.js");
+            yield task.start();
+        }
+        // =========== [ buildApp ] ===========
+        else if (["buildApp", "build"].indexOf(argv2) > -1) {
+            var task = require("./tasks/buildApp/index.js");
+            yield task.start();
+        }
 
         // =========== [ cli ] ===========
         else if (['cli','c'].indexOf(argv2) > -1) {
@@ -60,16 +36,16 @@ jobs.index = co.wrap(function*() {
 
         // =========== [ help ] ===========
         else {
-            var task = require("./tasks/help/index.js");
-            yield task.start(module_path);
+            require("dm-npm").getCommonTasks(argv2, __dirname);
         }
 
     } catch (e) {
         console.log("Filename: ", __filename, "\n", e.stack);
     }
 
-    return Promise.resolve(result);
+    return Promise.resolve();
 }); // job.index()
+
 
 // =========== [ MODULE EXPORT ] ===========
 module.exports = jobs;
