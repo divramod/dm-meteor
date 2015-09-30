00779f9d18bfe2cd3bab663455ed0fc8c5ccaed0
divramod <divramod@gmail.de>
2015-09-30 13:12:53 +0200

    before merge

--- tasks/routeAdd/templateBlaze/model/ROUTENAMEPLURAL.js
+++ tasks/routeAdd/templateBlaze/model/ROUTENAMEPLURAL.js
@@ -1,13 +1,13 @@
 ROUTENAMEPLURALCAPITALIZE = new Mongo.Collection("ROUTENAMEPLURAL");
 
 ROUTENAMEPLURALCAPITALIZE.allow({
-    insert: function(userId, ROUTENAME) {
-        return userId && ROUTENAME.owner === userId;
+    insert: function(userId, ROUTENAMEPLURAL) {
+        return userId && ROUTENAMEPLURAL.owner === userId;
     },
-    update: function(userId, ROUTENAME, fields, modifier) {
-        return userId && ROUTENAME.owner === userId;
+    update: function(userId, ROUTENAMEPLURAL, fields, modifier) {
+        return userId && ROUTENAMEPLURAL.owner === userId;
     },
-    remove: function(userId, ROUTENAME) {
-        return userId && ROUTENAME.owner === userId;
+    remove: function(userId, ROUTENAMEPLURAL) {
+        return userId && ROUTENAMEPLURAL.owner === userId;
     }
 });
